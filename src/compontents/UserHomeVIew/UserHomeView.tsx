bef730ddc9d0561622b3a76d41a02cd0d16dacb1
wojtekzielinski96@gmail.com <wojtekzielinski96@gmail.com>
2025-06-12 12:01:41 +0200

    add fetching user data and workouts

--- src/compontents/UserHomeVIew/UserHomeView.tsx
+++ src/compontents/UserHomeVIew/UserHomeView.tsx
@@ -1,16 +1,57 @@
+import { useQuery } from "@tanstack/react-query"
+
 
 type UserHomeViewProps = {
   authToken: string;
   userId: string;
 }
 
+const wait = (duration: number) => {
+  return new Promise(resolve => setTimeout(resolve, duration))
+}
+
+const fetchUser = async (authToken: string, userId: string) => {
+  await wait(1000);
+  const response = await fetch(`http://localhost:8080/api/users/${userId}`, {
+    headers: {
+      'authentication': `Bearer ${authToken}`
+    },
+  })
+  return await response.json();
+}
+
+const fetchWorkouts = async (authToken: string, userId: string) => {
+  await wait(1000);
+  const response = await fetch(`http://localhost:8080/api/users/${userId}/workouts`, {
+    headers: {
+      'authentication': `Bearer ${authToken}`
+    },
+  })
+  return await response.json();
+}
+
 export const UserHomeView = ({ authToken, userId }: UserHomeViewProps) => {
+
+  const userQuery = useQuery({
+    queryKey: ["user"],
+    queryFn: () => fetchUser(authToken, userId) 
+  })
+
+  const workoutsQuery = useQuery({
+    queryKey: ["workouts"],
+    queryFn: () => fetchWorkouts(authToken, userId)
+  })
 
   return (
     <>
       <h2>Welcome in Bulky Bull App - you are logged in</h2>
-      <p>{authToken}</p>
-      <p>{userId}</p>
+
+      {userQuery.isLoading && <p>Loading user...</p>}
+      {userQuery.data && <p>Hello {userQuery.data.firstName} {userQuery.data.lastName}</p>}
+
+      {workoutsQuery.isLoading && <p>Loading workouts...</p>}
+      {workoutsQuery.data && <p>Workouts number: {workoutsQuery.data.length}</p>}
+
     </>
   )
 }
